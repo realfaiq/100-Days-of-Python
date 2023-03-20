5ed53ccf5aff4f3a011218344bbfbf23732e338f
Faiq <ahmadfaiq46@gmail.com>
2023-03-20 23:28:36 +0500

    Day-1. Coded Variables and Data-types Primitive and Non-Primitive all about that

---
+++ Variables and Data-types .py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Primitive
+#integer variable
+count = 100
+print(count)
+
+
+# In[2]:
+
+
+#Primitive
+#String variable
+name = "Faiq Ahmad"
+print(name)
+
+
+# In[3]:
+
+
+#Primitive
+#float variable
+weather = 24.6
+weather
+
+
+# In[4]:
+
+
+#Primitive
+#boolean variable
+true = True
+true
+
+
+# In[6]:
+
+
+#Primitive
+name_of_city = "Peshawar"
+print(name_of_city)
+
+
+# In[10]:
+
+
+#Primitive
+#Tuple
+this_tuple = ("apple", "bananna", 1)
+this_tuple
+
+
+# In[9]:
+
+
+#Non-Primitive
+#List
+list1 = [True, 'apple', 12, 54]
+list1
+
+
+# In[11]:
+
+
+#Non-Prmitive
+#Array in Python
+fruits = ['apple', 'mango', 'banana', 'oranges']
+fruits
+
+
+# In[12]:
+
+
+#Non-Primitive
+#Class
+class Person:
+    name = "Faiq Ahmad"
+    age = "22"
+
+p1 = Person()
+print(p1.name)
+print(p1.age)
+
+
+# In[ ]:
+
+
+
+
