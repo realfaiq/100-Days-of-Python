76817826b0171bec5ef6084c94cf5b9858291e3a
Faiq <ahmadfaiq46@gmail.com>
2023-03-21 21:57:48 +0500

    Day-2 Coded about Operators, Operands and Input in python

---
+++ Operators, Operands and Input.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Expression
+2 + 3
+
+
+# In[4]:
+
+
+#Arithmetic Operators
+print(2 + 3)
+print(3 - 1)
+print(10 / 2)
+print(10 % 2)
+
+
+# In[5]:
+
+
+#Floor Operator
+10 // 3
+
+
+# In[11]:
+
+
+#Assignment Operators
+a = 34
+a -= 12 # a = a - 12
+a *= 12
+a /= 12
+print(a)
+
+
+# In[14]:
+
+
+#Comparison Operators
+# b = (14<=7)
+# b = (14>=7)
+# b = (14<7)
+# b = (14>7)
+#= (14==7)
+# = (14!=7)
+print(b)
+
+
+# In[15]:
+
+
+#Logical Operators
+bool1 = True
+bool2 = False
+print("The value of bool1 and bool2 is", (bool1 and bool2))
+print("The value of bool1 or bool2 is", (bool1 or bool2))
+print("The value of not bool2 is", (not bool2))
+
+
+# In[17]:
+
+
+#Another Example of Logical Operator
+temprature = 14
+raining = False
+outing = not raining and temprature < 20
+print(outing)
+
+
+# In[23]:
+
+
+#Inputs
+print('Enter a number')
+a = input()
+print('Enter another number')
+b = input()
+c = int(a) + int(b)
+print(c)
+
+
+# In[ ]:
+
+
+
+
